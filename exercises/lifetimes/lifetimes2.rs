4f080c5322ab13bc1dd082bf791503a275ca111c
kreel <stujyh@126.com>
2025-09-19 22:16:52 +0800

    finished half tests as requested

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -18,10 +16,13 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 fn main() {
     let string1 = String::from("long string is long");
+    // Solution one:
+    // + let mut string2
     let result;
     {
         let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
+        // Solution two: creating a new string on return of `longest`
+        result = String::from(longest(string1.as_str(), string2.as_str()));
     }
     println!("The longest string is '{}'", result);
 }
